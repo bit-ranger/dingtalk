5235bad06eb679c25ff29c8919b3cda8d4ec981a
wanxingahi <53387800+wanxingahi@users.noreply.github.com>
2021-09-16 15:04:23 +0800

    Update base.go
    1.添加日志框架logrus 2.推送钉钉消息改为markdown格式

--- src/dingtalk/base.go
+++ src/dingtalk/base.go
@@ -98,7 +98,8 @@ type GitlabWebhookModel struct {
 		Status string `json:"status"`
 	} `json:"object_attributes"`
 	Project struct {
-		Name string `json:"name"`
+		Name   string `json:"name"`
+		WebUrl string `json:"web_url"`
 	} `json:"project"`
 }
 
